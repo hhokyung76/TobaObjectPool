8d2d52ef75b566d9b36b5fd099c14dd23378319d
이호경 <hk.lee@tg360tech.com>
2022-05-12 15:04:33 +0900

    추가모듈 테스트

--- src/main/java/com/toba/pool/core/UUIDVaildatorPoolCallableTest.java
+++ src/main/java/com/toba/pool/core/UUIDVaildatorPoolCallableTest.java
@@ -17,28 +17,29 @@ public class UUIDVaildatorPoolCallableTest {
 
 	public static void main(String[] args) {
 		String checkTargetId = "06754b37-56fc-47bd-897f-10ca1ad1abe1";
-
+		int maxTotal = 3;
+		String startTime = ScStringUtils.getCurrentTimeOfLog();
 		GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-		config.setMaxTotal(6);
-		config.setMaxWaitMillis(50);
+		config.setMaxTotal(1);
+		//config.setMaxWaitMillis(50);
 		GenericObjectPool<UUIDUtil> uuidValidatorPool = new GenericObjectPool<UUIDUtil>(new UUIDVaildatorPool(), config);
 
-		for (int ii = 0; ii < 20; ii++) {
-			UUIDUtil util2 = null;
-			try {
-				util2 = uuidValidatorPool.borrowObject();
-				util2.isValidUUID("");
-				uuidValidatorPool.returnObject(util2);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+//		for (int ii = 0; ii < 20; ii++) {
+//			UUIDUtil util2 = null;
+//			try {
+//				util2 = uuidValidatorPool.borrowObject();
+//				util2.isValidUUID("");
+//				uuidValidatorPool.returnObject(util2);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
 		ExecutorService executor = Executors.newFixedThreadPool(8);
 
 		List<Callable<String>> valStatusJobs = new ArrayList<>();
 
 		String checkTargetId2 = "06754b37-56fc-47bd-897f-10ca1ad1";
-		for (int ii = 0; ii < 3000; ii++) {
+		for (int ii = 0; ii < 300; ii++) {
 			String temp = StringUtils.leftPad(Integer.toString(ii), 4, "0");
 			String checkTargetId3 = checkTargetId2 + temp;
 			UuidValidatorCallable callable = new UuidValidatorCallable(uuidValidatorPool, checkTargetId3);
@@ -62,9 +63,12 @@ public class UUIDVaildatorPoolCallableTest {
 			}
 		}
 		executor.shutdown();
+
+		String endTime = ScStringUtils.getCurrentTimeOfLog();
 		System.out.println("-----------------------------------------------------------");
 
-
+		log.info("## StartTime: "+startTime);
+		log.info("##   EndTime: "+endTime);
 
 	}
 
